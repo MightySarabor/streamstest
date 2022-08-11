973f4786e11a31b316bb9f1cac7a3f7313eeca30
MightySarabor <fleschm@hochschule-trier.de>
2022-08-11 18:04:22 +0200

    changed minor things, last try

--- streamstest/src/main/java/de/hstr/bigdata/WordCount.java
+++ streamstest/src/main/java/de/hstr/bigdata/WordCount.java
@@ -32,15 +32,13 @@ public class WordCount {
         // Code zu Ende
         //Propoerties
         Properties config = new Properties();
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "filter-warnings");
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount");
         config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
                 "infbdt07.fh-trier.de:6667,infbdt08.fh-trier.de:6667,infbdt09.fh-trier.de:6667");
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
         config.put("security.protocol", "SASL_PLAINTEXT");
-        config.put("enable.auto.commit", "true");
-        config.put("auto.commit.interval.ms", "1000");
         //Properties zu Ende
 
         //Topology beschreiben
