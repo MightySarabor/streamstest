bf640350788b2eda04054b76878fdb123fc3a205
MightySarabor <fleschm@hochschule-trier.de>
2022-08-26 17:28:00 +0200

    reformatted Stateful example to make it easier to read and modify

--- streamstest/src/main/java/de/hstr/bigdata/StatefulExample.java
+++ streamstest/src/main/java/de/hstr/bigdata/StatefulExample.java
@@ -1,17 +1,21 @@
 package de.hstr.bigdata;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
+
 
 /**
  * export KAFKA_OPTS="-Djava.security.auth.login.config=$PWD/kafka.jaas"
@@ -26,15 +30,30 @@ import org.apache.kafka.streams.kstream.Produced;
  */
 public class StatefulExample {
 
+    static void runKafkaStreams(final KafkaStreams streams) {
+        final CountDownLatch latch = new CountDownLatch(1);
+        streams.setStateListener((newState, oldState) -> {
+            if (oldState == KafkaStreams.State.RUNNING && newState != KafkaStreams.State.RUNNING) {
+                latch.countDown();
+            }
+        });
+
+        streams.start();
+
+        try {
+            latch.await();
+        } catch (final InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.err.printf("Streams Closed");
+    }
     private static KafkaStreams streams;
-
-    public static void main(String[] args) {
-        // Achtung, Datei kafka.jaas muss angepasst werden!
+    static Topology buildTopology(String inputTopic, String outputTopic) {
         System.setProperty("java.security.auth.login.config", "/home/fleschm/kafka.jaas");
-
         StreamsBuilder builder = new StreamsBuilder();
 
-        final KStream<String, String> textLines = builder.stream("fleschm-1");
+        final KStream<String, String> textLines = builder.stream(inputTopic);
         KTable<String, Long> count = textLines.flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
                 .peek((k, v) -> System.err.printf("Word: (%s, %s)\n", k, v))
                 .groupBy((key, value) -> value)
@@ -42,39 +61,46 @@ public class StatefulExample {
 
         count.toStream()
                 .peek((key, value) -> System.err.printf("Outgoing record (%s, %s)\n", key, value))
-                .to("fleschm-2", Produced.with(Serdes.String(), Serdes.Long()));
+                .to(outputTopic, Produced.with(Serdes.String(), Serdes.Long()));
 
         Properties config = new Properties();
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "fleschm-test");
-        config.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 2);
-        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "infbdt07.fh-trier.de:6667,infbdt08.fh-trier.de:6667,infbdt09.fh-trier.de:6667");
-        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        config.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        config.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        config.put("security.protocol", "SASL_PLAINTEXT");
-        config.put("enable.auto.commit", "true");
-        config.put("auto.commit.interval.ms", "100");
-
-        System.err.println("<--- Stateful Example --->");
-        System.err.println("Building topology.");
-        Topology topology = builder.build();
-
-        streams = new KafkaStreams(topology, config);
-
-        System.err.println("Starting Kafka Streams.");
-        Util utility = new Util();
-        Util.Randomizer rando = utility.startNewRandomizer(config, "fleschm-1");
-        streams.start();
-
-        Runtime.getRuntime().addShutdownHook(new Thread(StatefulExample::shutdown));
+        return builder.build();
     }
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            throw new IllegalArgumentException("This program takes one argument: the path to a configuration file.");
+        }
+        Properties props = new Properties();
+        try (InputStream inputStream = new FileInputStream(args[0])) {
+            props.load(inputStream);
+        }
 
-    public static void shutdown() {
-        if (streams != null) {
-            System.err.println("Shutting down Kafka Streams.");
-            streams.close();
+        final String inputTopic = props.getProperty("input.topic.name");
+        final String outputTopic = props.getProperty("output.topic.name");
+
+        try (Util utility = new Util()) {
+
+            utility.createTopics(
+                    props,
+                    Arrays.asList(
+                            new NewTopic(inputTopic, Optional.empty(), Optional.empty()),
+                            new NewTopic(outputTopic, Optional.empty(), Optional.empty())));
+
+            // Ramdomizer only used to produce sample data for this application, not typical usage
+            try (Util.Randomizer rando = utility.startNewRandomizer(props, inputTopic)) {
+                System.err.println("Building topology.");
+                KafkaStreams kafkaStreams = new KafkaStreams(
+                        buildTopology(inputTopic, outputTopic),
+                        props);
+
+                Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+
+                System.err.println("Starting Kafka Streams.");
+
+                runKafkaStreams(kafkaStreams);
+                System.err.println("Shutting down Kafka Streams.");
+
+            }
         }
     }
 }
