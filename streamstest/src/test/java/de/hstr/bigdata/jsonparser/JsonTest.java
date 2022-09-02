dca09b403a2922697b65f5b221945e926d5ba6c5
MightySarabor <fleschm@hochschule-trier.de>
2022-09-02 14:00:12 +0200

    adasdasd

--- streamstest/src/test/java/de/hstr/bigdata/jsonparser/JsonTest.java
+++ streamstest/src/test/java/de/hstr/bigdata/jsonparser/JsonTest.java
@@ -2,6 +2,8 @@ package de.hstr.bigdata.jsonparser;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import de.hstr.bigdata.jsonparser.pojo.AuthorPOJO;
+import de.hstr.bigdata.jsonparser.pojo.BookPOJO;
 import de.hstr.bigdata.jsonparser.pojo.DayPOJO;
 import de.hstr.bigdata.jsonparser.pojo.SimpletestCaseJsonPOJO;
 import org.junit.jupiter.api.Test;
@@ -21,7 +23,22 @@ class JsonTest {
             "  \"date\": \"2019-12-25\",\n" +
             "  \"name\": \"Christmas Day\"\n" +
             "}";
-
+    private String authorBookScenario ="{\n" +
+            "  \"authorName\": \"Max\",\n" +
+            "  \"books\": [\n" +
+            "    {\n" +
+            "      \"title\": \"title1\",\n" +
+            "      \"inPrint\": true,\n" +
+            "      \"publishDate\": \"2019-12-25\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"title\": \"title2\",\n" +
+            "      \"inPrint\": false,\n" +
+            "      \"publishDate\": \"2019-01-01\"\n" +
+            "    }\n" +
+            "\n" +
+            "  ]\n" +
+            "}";
     @Test
     void parse() throws IOException {
 
@@ -70,5 +87,18 @@ class JsonTest {
         DayPOJO pojo = Json.fromJson(node, DayPOJO.class);
 
         assertEquals("2019-12-25", pojo.getDate().toString());
+    }
+
+    @Test
+    void setAuthorBookScenario1() throws IOException {
+        JsonNode node = Json.parse(authorBookScenario);
+        AuthorPOJO pojo = Json.fromJson(node, AuthorPOJO.class);
+
+        System.out.println("Author : " + pojo.getAuthorName());
+        for(BookPOJO bP : pojo.getBooks()){
+            System.out.println("Book : "+ bP.getTitle());
+            System.out.println("is in print? : "+ bP.isInPrint());
+            System.out.println("Publish Date : "+ bP.getPublishDate());
+        }
     }
 }
